95f90c85993607fdabc2b338e1fbb7b6106da4c5
arshad <skarshad@outlook.com>
2025-06-01 13:46:10 +0530

    libuv event loop cycle example and libuv cycle diag for ref

--- libuv-ex.js
+++ libuv-ex.js
@@ -1,3 +1,13 @@
 const fs = require("node:fs");
 
-fs.readFile("readFile.txt","utf-8",()=> console.log("ReadFile"));
+fs.readFile("readFile.txt","utf-8",(e,data)=> {
+    console.log(data);
+});
+
+setImmediate(()=> console.log("setImmediate"));
+setTimeout(()=> console.log("setTimeout"));
+console.log("libv-ex");
+Promise.resolve("resolved promise").then(console.log);
+
+
+//refer libuv working cycle for understanding its printing patter - libuv-eventloop-cycle.png
